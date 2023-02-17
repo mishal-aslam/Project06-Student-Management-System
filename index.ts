7482675f1fa2f968a48ad6d1cbf4a2e5639b0f84
Mishal Aslam <101046955+mishal-aslam@users.noreply.github.com>
2023-02-17 23:49:22 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+
+
+import inquirer from 'inquirer';
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'paragraph',
+      message: 'Enter a paragraph of text:',
+    },
+  ])
+  .then((answers) => {
+    const paragraph = answers.paragraph;
+    const words = paragraph;
+    const wordCount = words.split(" ").length;
+    const characterCount = words.replace(/\s/g, '').length;
+
+    console.log(`The paragraph has ${wordCount} words and ${characterCount} characters without whitespaces.`);
+  });
+
+
+
+
